9dedc4a63301c1c0f714e29527635b47a38252ba
Heli4m <ngohailam2010@gmail.com>
2026-02-19 11:16:03 +0700

    Initial Commit

---
+++ MinimalFlashcards/MinimalFlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalFlashcardsApp.swift
+//  MinimalFlashcards
+//
+//  Created by Liam Ngo on 19/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MinimalFlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
