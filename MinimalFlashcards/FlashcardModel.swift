ed1cb22e95716151070f46ad99172ecf704c0311
Heli4m <ngohailam2010@gmail.com>
2026-02-19 17:54:20 +0700

    Created FlashcardModel

--- MinimalFlashcards/FlashcardModel.swift
+++ MinimalFlashcards/FlashcardModel.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct FlashcardModel: View {
+    let clue: String
+    let answer: String
+    @State private var rotation: Double = 0
+    @State private var isFlipped: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 300, height: 400)
+                .foregroundStyle(Config.Colors.item)
+                .onTapGesture { tap in
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        rotation += 180
+                        isFlipped.toggle()
+                    }
+                }
+                .rotation3DEffect(.degrees(rotation), axis: (x: 0, y: 1, z: 0))
+            
+            if isFlipped {
+                LexendMediumText(text: answer, size: 20)
+                    .foregroundStyle(Config.Colors.primaryText)
+            } else if !isFlipped {
+                LexendMediumText(text: clue, size: 20)
+                    .foregroundStyle(Config.Colors.primaryText)
+            }
+        }
     }
 }
 
 #Preview {
-    FlashcardModel()
+    FlashcardModel(clue: "Single Bedroom", answer: "das Einzelzimmer")
 }
